307acffbd19a5b875333d89f0df587ac81b3cc26
istiak2426 <istiak2426@gmail.com>
2024-03-17 11:37:28 +0600

    add static method

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -1,2 +1,20 @@
-﻿Console.WriteLine("lolololo");
-    
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+            MyMethod();
+            MyMethod();
+        }
+    }
+}
+
